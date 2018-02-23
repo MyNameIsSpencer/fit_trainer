276b54336e718b8786628ffc7c4160b1ceb98feb
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-22 19:41:26 -0500

    feb 22 at 7 41

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component{
   }
 
   openMenu = () => {
-    debugger;
     this.setState({
       isMenuOpen: !this.state.isMenuOpen
     });
@@ -27,48 +26,37 @@ class App extends Component{
   return (<div>
     <header>
       <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <nav className="nav-header">
-            <Link to="/" className={"list-item"}>Home</Link>
-            <Link to="/services" className={"list-item"}>Services</Link>
-            <Link to="/about" className={"list-item"}>About</Link>
-            <Link to="/contact" className={"list-item"}>Contact</Link>
-          </nav>
-        </div>
-        <div className="hamburg-container" onClick={this.openMenu}>
-          <div className="hamburg-paddie"> </div>
-          <div className="hamburg-paddie"> </div>
-          <div className="hamburg-paddie"> </div>
-        </div>
-        {
-          this.state.isMenuOpen? (
-                <div className="hamburger-dropdown">
-              <nav className="small-nav-header">
-                <Link to="/" className={"list-item"}>Home</Link>
-                <Link to="/services" className={"list-item"}>Services</Link>
-                <Link to="/about" className={"list-item"}>About</Link>
-                <Link to="/contact" className={"list-item"}>Contact</Link>
-              </nav>
-            </div>
-          ): null
-        }
-        <div>
-          <nav className="nav-header">
-            <Link to="/" className={"list-item"}>Home</Link>
-            <Link to="/services" className={"list-item"}>Services</Link>
-            <Link to="/about" className={"list-item"}>About</Link>
-            <Link to="/contact" className={"list-item"}>Contact</Link>
-          </nav>
-        </div>
-        <div>
-        <div>Menu Area</div>
-          <Route exact path="/"component={Home} />
-          <Route path="/services" component={Services} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-
-        </div>
+      <nav className="nav-header">
+        <Link to="/" className={"list-item"}>Home</Link>
+        <Link to="/services" className={"list-item"}>Services</Link>
+        <Link to="/about" className={"list-item"}>About</Link>
+        <Link to="/contact" className={"list-item"}>Contact</Link>
+      </nav>
+      <div className="hamburg-container" onClick={this.openMenu}>
+        <div className="hamburg-paddie"> </div>
+        <div className="hamburg-paddie"> </div>
+        <div className="hamburg-paddie"> </div>
+      </div>
+      {
+      this.state.isMenuOpen? (
+          <div className="hamburger-dropdown">
+            <nav className="small-nav-header">
+              <Link to="/" className={"drop-item"}>Home</Link>
+              <Link to="/services" className={"drop-item"}>Services</Link>
+              <Link to="/about" className={"drop-item"}>About</Link>
+              <Link to="/contact" className={"drop-item"}>Contact</Link>
+            </nav>
+          </div>
+        ): null
+      }
       </header>
+      <div>
+      <Route exact path="/"component={Home} />
+      <Route path="/services" component={Services} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+
+      </div>
     <div>
     </div>
 
