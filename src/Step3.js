229518de370456c34808983e85c33b6722f0ff15
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-04-24 01:37:18 -0400

    no more blue or red branch

--- src/Step3.js
+++ src/Step3.js
@@ -3,14 +3,23 @@ import './css/Step3.css';
 
 class Step3 extends Component {
   constructor(props) {
-    super(props);
-    
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      goal: '',
+      plan: '',
+      message: ''
+    }
+
   }
 
 
   render() {
     return(
-
     )
   }
 }
+
+export default Step3
