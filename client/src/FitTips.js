bcf09272a76c7fa46144aed5708698abc326f582
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-06-02 18:52:08 -0400

    change fit tips links a little

--- client/src/FitTips.js
+++ client/src/FitTips.js
@@ -15,8 +15,7 @@ class FitTips extends Component {
       <div>
         <Youtuber url="https://www.youtube.com/embed/nJEP2CE8oGM" title="Chair Exercise" description="Turn your chair into a grueling training regimen"/>
         <Youtuber url="https://www.youtube.com/embed/6gOv516iooU"  title="Seated Hip Flexion" description="Sit on the chair and get those hips flexing"/>
-        <Youtuber url="https://www.youtube.com/embed/T5CYbfByXPo" title="Band Exercises" description="Lot's o exercises with bands that will get you burning!"/>
-        <Youtuber url="https://www.youtube.com/embed/B5eAei_ISG4" title="Flexor Training" description="Lot's and LOT'S of Flexon, get your Flex ON!"/>
+        <Youtuber url="https://www.youtube.com/embed/mQLzNf8VOIc" title="Band Exercises" description="Lot's o exercises with bands that will get you burning!"/>
       </div>
     )
   }
